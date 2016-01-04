ddea4cc8cae8b0df54fec2774193e8efb2a3ad63
U-NRPI\nbodepalli.ctr <nbodepalli.ctr@auction.com>
2016-01-04 11:58:45 -0800

    Added forcast and exception handling

--- usdinr.py
+++ usdinr.py
@@ -1,3 +1,4 @@
+import datetime
 import jinja2
 import json
 import os
@@ -28,6 +29,11 @@ class RateHistory(ndb.Model):
     ex = ndb.StringProperty(indexed=False)
     date = ndb.DateTimeProperty(auto_now_add=True)
 
+class MonthMax(ndb.Model):
+    """A main model for representing an individual Guestbook entry."""
+    max_rate = ndb.FloatProperty(indexed=False)
+    date = ndb.DateTimeProperty()
+    max_date = ndb.DateTimeProperty(auto_now_add=True)
 
 class MainPage(webapp2.RequestHandler):
 
@@ -47,6 +53,22 @@ class MainPage(webapp2.RequestHandler):
         history.ex = value['today']
         history.put()
 
+
+        month_date = datetime.datetime.strptime("%s-%s-2"%(datetime.date.today().year, datetime.date.today().month), '%Y-%m-%d')
+        results = MonthMax.query(MonthMax.date == month_date).fetch()
+        high_float_value = float(value['high'])
+        if results:
+            if results[0].max_rate < high_float_value:
+                results[0].max_rate = high_float_value
+                results[0].put()
+        else:
+            maxi = MonthMax()
+            maxi.max_rate = high_float_value
+            maxi.date = month_date
+            maxi.put()
+
+        results = MonthMax.query().fetch()
+
         rates_query = RateHistory.query().order(-RateHistory.date)
         rates = rates_query.fetch(100)
 
@@ -54,7 +76,8 @@ class MainPage(webapp2.RequestHandler):
 
         template_values = {
             'money' : {'yahoo': yahoo},
-            'rates': rates
+            'rates': rates,
+            'results': results
         }
 
         template = JINJA_ENVIRONMENT.get_template('index.html')
@@ -64,32 +87,33 @@ class MainPage(webapp2.RequestHandler):
 class Real(webapp2.RequestHandler):
 
     def getCRUSDINR(self, url):
-        result = urlfetch.fetch(url)
-        p = re.search('Rate <img src="images/icon_rupees.jpg" style="vertical-align:text-top;">.*', result.content)
-        t =  p.group()
-        p = re.search('[0-9]+\.[0-9]+', t)
-        return p.group()
+        value = "N/A"
+        try:
+            result = urlfetch.fetch(url)
+            p = re.search('Rate <img src="images/icon_rupees.jpg" style="vertical-align:text-top;">.*', result.content)
+            t =  p.group()
+            p = re.search('[0-9]+\.[0-9]+', t)
+            value = p.group()
+        except:
+            pass
+        return value
 
     def getCRTopUSDINR(self):
         rate = urlfetch.fetch('http://www.compareremit.com/calculation/today_exchange_rate/1').content
         provider = urlfetch.fetch('http://www.compareremit.com/calculation/today_exchange_rate/4').content
-        return {'rate': rate, 'provider': provider}
+        return {'label': provider, 'value': rate, 'ref': ''}
 
     def get(self):
         ria = self.getCRUSDINR('http://www.compareremit.com/money-transfer-companies/ria-money-transfer/')
         western_union = self.getCRUSDINR('http://www.compareremit.com/money-transfer-companies/western-union/')
         transwise = self.getCRUSDINR('http://www.compareremit.com/money-transfer-companies/transferwise/')
         xoom = self.getCRUSDINR('http://www.compareremit.com/money-transfer-companies/xoom/')
-        top = self.getCRTopUSDINR()
-        resp = {
-            'top': top,
-            'rates': [
-                {'label': 'WU', 'value': western_union},
-                {'label': 'Ria', 'value': ria},
-                {'label': 'TransferWise', 'value': transwise},
-                {'label': 'Xoom', 'value': xoom}
-            ]
-        };
+        resp = [
+                {'label': 'WU', 'value': western_union,'ref': 'https://refer.westernunion.com/mt-mgm-us/recipient'},
+                {'label': 'Ria', 'value': ria, 'ref': 'http://refer.riamoneytransfer.com/v2/share/6189557156121780477/6c616b73686d6973616e746869707269796140676d61696c2e636f6d'},
+                {'label': 'TransferWise', 'value': transwise, 'ref': 'https://transferwise.com/u/66beb6'},
+                {'label': 'Xoom', 'value': xoom, 'ref': 'http://refer.xoom.com/v2/share/6182101262079424292'},
+        ];
         self.response.out.write(json.dumps(resp))
         
 
